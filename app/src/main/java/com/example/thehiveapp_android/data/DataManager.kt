9b3ef4b118f09362474f768fe5d5feed9a538631
Zaccari S <zacosx@gmail.com>
2019-09-26 18:13:00 -0400

    Playing around with Realm a bit.

--- app/src/main/java/com/example/thehiveapp_android/data/DataManager.kt
+++ app/src/main/java/com/example/thehiveapp_android/data/DataManager.kt
@@ -1,10 +1,36 @@
 package com.example.thehiveapp_android.data
 
+import io.realm.Realm
+import io.realm.RealmObject
+
 class DataManager {
-    //The closest thing we get to static methods in Kotlin.
+    //Let's make this a singleton class...
     companion object {
+        private lateinit var managerInstance : DataManager
 
-
+        val instance: DataManager
+            get() {
+                if (managerInstance == null){
+                    managerInstance = DataManager()
+                }
+                return managerInstance
+            }
 
     }
+
+    private var realm : Realm = Realm.getDefaultInstance()
+
+
+    fun saveObject(saveMe: RealmObject){
+
+        //If we make the RealmObject first,
+        //we should (in theory) pass it in and then use copyToRealm to save it to the database.
+        realm.executeTransactionAsync { realm ->
+            realm.copyToRealm()
+        }
+    }
+
+    // private constructor()
+
+
 }
--- app/src/main/java/com/example/thehiveapp_android/data/HiveRealmObject.kt
+++ app/src/main/java/com/example/thehiveapp_android/data/HiveRealmObject.kt
@@ -1,8 +1,28 @@
 package com.example.thehiveapp_android.data
 
-// import org.jetbrains.anko.doAsync
-import io.realm.*
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.Date
 
-class HiveRealmObject(var name: String) : RealmObject() {
-    
+//RealmObject() descendants must be open classes; Kotlin classes are final by default
+open class HiveRealmObject() : RealmObject() {
+    //These do actually need default values, even if they're optional...
+    @PrimaryKey
+    var uuid: Long = 0
+    var name: String = "My Hive"
+    var createdAt: Date = Date()
+    var lastAttendedAt: Date = Date() //Date() takes on the "current" date (i.e when it's called).
+    //Much of this is "when was the last time I did ___?", so we're going to need to save many dates
+
+    //For the rest of this, I'm honestly spitballing a bit.
+    var estimatedPopulation: Int? = null
+    var queenBirthday: Date? = null
+    var lastMiteInspection: Date? = null
+    var honeyLastHarvestedAt: Date? = null
+
+    //var location: IDontKnowWhatType? = null
+    // What we should do probably depends on where we are because of temperature.
+
+
+
 }
