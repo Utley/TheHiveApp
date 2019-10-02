f3b686ea68fac45331901c7b7d6be0409d06e6ce
Stephen Utley <utley2600@gmail.com>
2019-10-02 14:26:54 -0400

    make test activity look better

--- app/src/main/java/com/example/thehiveapp_android/MainActivity.kt
+++ app/src/main/java/com/example/thehiveapp_android/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.havigation_hive_diagram, R.id.navigation_notifications, R.id.navigation_hive_form
+                R.id.navigation_home, R.id.navigation_hive_diagram, R.id.navigation_notifications, R.id.navigation_hive_form
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
