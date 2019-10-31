2b90d4b7391c0c3ac0b73c6c0e19428ad19d1f1c
cole <gregoc3@rpi.edu>
2019-10-31 19:14:48 -0400

    Add save functionality to the HiveForm

--- app/src/main/java/com/example/thehiveapp_android/ui/hiveForm/HiveFormFragment.kt
+++ app/src/main/java/com/example/thehiveapp_android/ui/hiveForm/HiveFormFragment.kt
@@ -1,15 +1,17 @@
 package com.example.thehiveapp_android.ui.hiveForm
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-
+import androidx.fragment.app.Fragment
 import com.example.thehiveapp_android.R
+import com.example.thehiveapp_android.data.DataManager
+import com.example.thehiveapp_android.data.HiveRealmObject
+import java.util.*
 
 class HiveFormFragment : Fragment() {
 
@@ -17,25 +19,28 @@ class HiveFormFragment : Fragment() {
         fun newInstance() = HiveFormFragment()
     }
 
-    private lateinit var viewModel: HiveFormViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.hive_form_fragment, container, false)
 
-//        val textView: TextView = root.findViewById(R.id.hive_form_title_default)
-        val textView: EditText = root.findViewById(R.id.hiveNameTextInput)
-        textView.setText("hive 1")
+        val hiveName: EditText = root.findViewById(R.id.hiveNameTextInput)
+
+        val button = root.findViewById(R.id.saveButton) as Button;
+        button.setOnClickListener{
+            Log.d("HiveFormFragment", hiveName.text.toString());
+            val newHive = HiveRealmObject()
+            newHive.uuid = UUID.randomUUID().mostSignificantBits
+            newHive.name = hiveName.text.toString()
+            DataManager.instance.saveObject(newHive)
+        }
 
         return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(HiveFormViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
