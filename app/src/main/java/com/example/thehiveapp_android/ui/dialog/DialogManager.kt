95f2adf9112b9845938eeaae4586e522b6bee370
Zaccari S <zacosx@gmail.com>
2019-11-25 18:59:36 -0500

    Added DialogManager, to help make simple dialog boxes.

---
+++ app/src/main/java/com/example/thehiveapp_android/ui/dialog/DialogManager.kt
@@ -0,0 +1,36 @@
+package com.example.thehiveapp_android.ui.dialog
+
+import android.app.AlertDialog
+import android.content.Context
+import android.widget.Toast
+
+class DialogManager {
+
+    // Let's make this a singleton class...
+    companion object {
+        private var managerInstance : DialogManager? = null
+
+        val instance: DialogManager
+            get() {
+                if (managerInstance == null){
+                    managerInstance = DialogManager()
+                }
+                return managerInstance!!
+            }
+    }
+
+    fun presentDialog(context: Context, title: String, message: String){
+        val builder = AlertDialog.Builder(context) //Create our AlertDialog Builder.
+
+        builder.setTitle(title) //Set the title and message.
+        builder.setMessage(message)
+
+        builder.setNeutralButton("OK") { dialog, which ->
+            Toast.makeText(context, "OK", Toast.LENGTH_SHORT).show()
+        }
+
+        builder.show()
+
+    }
+
+}
