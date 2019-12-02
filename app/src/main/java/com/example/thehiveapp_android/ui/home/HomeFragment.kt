c1e54c29e6e4bbf281303fdce860f54b6b079753
cole <gregoc3@rpi.edu>
2019-12-02 18:03:35 -0500

    Rename padding in dimens.xml and fields in inspection form

--- app/src/main/java/com/example/thehiveapp_android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/thehiveapp_android/ui/home/HomeFragment.kt
@@ -8,15 +8,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ArrayAdapter
 import android.widget.EditText
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import com.example.thehiveapp_android.R
 import com.example.thehiveapp_android.data.DataManager
 import com.example.thehiveapp_android.data.HiveRealmObject
-import kotlinx.android.synthetic.*
 import android.widget.Spinner;
 import com.example.thehiveapp_android.ui.dialog.DialogManager
 
@@ -53,7 +50,7 @@ class HomeFragment : Fragment() {
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         // val root = inflater.inflate(R.layout.fragment_home, container, false)
         val root = inflater.inflate(R.layout.hive_form_fragment, container, false)
-        val hiveName: EditText = root.findViewById(R.id.hiveNameTextInput)
+        val hiveName: EditText = root.findViewById(R.id.nameInput)
         // Create an ArrayAdapter using the string array and a default spinner layout
         val hiveSizeSpinner : Spinner = root.findViewById(R.id.hiveSizeInput)
         ArrayAdapter.createFromResource(
