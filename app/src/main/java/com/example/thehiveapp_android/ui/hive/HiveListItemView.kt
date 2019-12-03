f42da9c91b6fa1ce87273d0ab09e6a7502fc298f
cole <gregoc3@rpi.edu>
2019-12-03 17:33:37 -0500

    Refactor HiveListItemView

--- app/src/main/java/com/example/thehiveapp_android/ui/hive/HiveListItemView.kt
+++ app/src/main/java/com/example/thehiveapp_android/ui/hive/HiveListItemView.kt
@@ -2,8 +2,6 @@ package com.example.thehiveapp_android.ui.hive
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
-import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.thehiveapp_android.R
@@ -11,30 +9,18 @@ import com.example.thehiveapp_android.data.HiveRealmObject
 import java.text.DateFormat
 
 /**
- * Displays individual hive info
- *
- * Displays the details of an individual hive in the context of a HiveListFragment.
- *
- * @author I forgot how to check
+ * The layout for an individual hive item in hive in the HiveListFragment list view.
  */
 class HiveListItemView : LinearLayout {
     private lateinit var hiveNameTextView: TextView
     private lateinit var hiveLastInspectedAtTextView: TextView
 
 
-    constructor(context: Context) : super(context){
+    constructor(context: Context) : super(context)
 
-    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-    @Suppress("UNUSED_PARAMETER")
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    constructor(context: Context, attrs: AttributeSet, defaultAttrsId: Int) : super(context, attrs, defaultAttrsId) {
-
-    }
+    constructor(context: Context, attrs: AttributeSet, defaultAttrsId: Int) : super(context, attrs, defaultAttrsId)
 
     /**
      * Populates this object with hive information.
@@ -42,8 +28,6 @@ class HiveListItemView : LinearLayout {
      * Populates this object with the information for a single hive, including filling the appropriate views.
      */
     fun populate(hive: HiveRealmObject){
-        //This is where we actually populate the views in this item.
-
         hiveNameTextView = this.findViewById(R.id.inspections) as TextView
         hiveLastInspectedAtTextView = this.findViewById(R.id.hive_last_inspected_text) as TextView
 
@@ -57,21 +41,4 @@ class HiveListItemView : LinearLayout {
 
         hiveLastInspectedAtTextView.text = hiveDateString
     }
-
-    /** TODO what does this do?? */
-    fun openHiveDetail(view: View) {
-
-    }
-
-
-    /**
-     * Specifies the view's behavior when clicked.
-     *
-     * @param v The view
-     */
-    fun onClick(v: View) {
-        Log.d("HiveListItemView", "Clicked Hive")
-    }
-
-
 }
